524e449409631f0aec401a351d165eaf8bc8c481
Mohamad Mahdi Tasha <imwhdiiii@gmail.com>
2023-03-26 15:52:36 +0430

    added checkbox component

--- src/components/components/iconComponent.js
+++ src/components/components/iconComponent.js
@@ -60,5 +60,18 @@ export default function IconComponent({type, isFilled}) {
                 <path d="M12.96 14H9.028v-.691l2.579-3.72v-.054H9.098v-.867h3.785v.691l-2.567 3.72v.054h2.645V14zM4.5 2.5a.5.5 0 0 0-1 0v9.793l-1.146-1.147a.5.5 0 0 0-.708.708l2 1.999.007.007a.497.497 0 0 0 .7-.006l2-2a.5.5 0 0 0-.707-.708L4.5 12.293V2.5z"/>
             </svg>
         );
+    } else if (type === 'circle') {
+        return (
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-circle" viewBox="0 0 16 16">
+                <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+            </svg>
+        );
+    } else if (type === 'checked-circle') {
+        return (
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-check2-circle" viewBox="0 0 16 16">
+                <path d="M2.5 8a5.5 5.5 0 0 1 8.25-4.764.5.5 0 0 0 .5-.866A6.5 6.5 0 1 0 14.5 8a.5.5 0 0 0-1 0 5.5 5.5 0 1 1-11 0z"/>
+                <path d="M15.354 3.354a.5.5 0 0 0-.708-.708L8 9.293 5.354 6.646a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l7-7z"/>
+            </svg>
+        );
     }
 }
--- src/components/components/taskComponent.js
+++ src/components/components/taskComponent.js
@@ -1,13 +1,14 @@
 // Codes By Mahdi Tasha
 // Importing Part
 import IconComponent from "./iconComponent";
+import TaskCheckboxComponent from "./taskCheckboxComponent";
 
 // Exporting Task Component Which Is Functional Component As Default
 export default function TaskComponent({name}) {
     // Returning JSX
     return(
         <div className='todo-app__task'>
-            <input className='todo-app__task-checkbox' type="checkbox"/>
+            <TaskCheckboxComponent />
             <p className='todo-app__task-title'>{name}</p>
             <button className='todo-app__task-star-btn'><IconComponent type={'star'}/></button>
         </div>
