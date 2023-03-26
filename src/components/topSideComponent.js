903ff2d76fe243a0273ca2aac18ff0fbd510ae29
Mohamad Mahdi Tasha <imwhdiiii@gmail.com>
2023-03-26 15:12:22 +0430

    impemented state in top side component

--- src/components/topSideComponent.js
+++ src/components/topSideComponent.js
@@ -2,23 +2,28 @@
 // Importing Part
 import IconComponent from "./components/iconComponent";
 import DropdownComponent from "./components/dropdownComponent";
+import { useSelector } from "react-redux";
 
 // Exporting Top Side Of App Which Is Functional Component As Default
 export default function TopSideComponent() {
+    // Using Hooks
+    const state = useSelector(store => store);
+    const dateState = state.date;
+
     // Returning JSX
     return(
         <div>
             <div>
                 <div>
                     <h6>
-                        <IconComponent type={'sun'} />
-                        My Day
+                        <IconComponent type={(dateState.isDay) ? 'sun' : 'moon'} />
+                        My {(dateState.isDay) ? 'Day' : 'Night'}
                     </h6>
                     <DropdownComponent type={'default'}>
                         <li><button>Print Items</button></li>
                     </DropdownComponent>
                 </div>
-                <h6>Sunday,March 26</h6>
+                <h6>{dateState.dayOfWeek},{dateState.monthName} {dateState.day}</h6>
             </div>
             <DropdownComponent type={'sort'}/>
         </div>
